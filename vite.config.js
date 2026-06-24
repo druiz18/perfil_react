f9ae3580eedd5004fa7a43ce5c464c940f0b6a86
DIEGO RUIZ <d.ruiz2257@gmail.com>
2026-06-24 11:19:48 -0500

    Configuración url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://druiz18.github.io/perfil_react.git',
+  base: '/perfil_react/',
 })
